eff9499e4d98cf30c6574e59ce2cd970efc57f87
Ruslan Salimov <work@salimov-dev.ru>
2022-08-31 13:30:20 +0300

    js-form

--- js/main.js
+++ js/main.js
@@ -1,48 +1,11 @@
-$(document).ready(function() {
+// $(document).ready(function() {
 
-  // --- SLIDER ---
+// });
 
-    $('.content__workmans').slick({
-        infinite: true,
-        slidesToShow: 4,
-        slidesToScroll: 4,
-        dots: true,
-        adaptiveHeight: true,
-        speed: 800,
-        autoplay: false,
-        autoplaySpeed: 2500,
-        responsive: [
-          {
-            breakpoint: 1367,
-            settings: {
-              slidesToShow: 3,
-              slidesToScroll: 2,
-              infinite: true,
-              dots: true
-            }
-          },
-          {
-            breakpoint: 550,
-            settings: {
-              slidesToShow: 1,
-              slidesToScroll: 1
-            }
-          },
-          {
-            breakpoint: 650,
-            settings: {
-              slidesToShow: 2,
-              slidesToScroll: 1
-            }
-          }
-        ]
-      });
 
-// --- BURGER ---
+let searchForm = document.querySelector('#input')
+let reg = /!@#$%^&*()/
 
-      $(".header__burger").click(function(event) {
-        $(".header__burger, .header__nav").toggleClass("active")
-
-      });
-
-});
+searchForm.oninput = function() {
+  this.value = this.value.replace(reg, '')
+}
